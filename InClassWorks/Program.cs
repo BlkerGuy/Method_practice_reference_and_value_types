83efdd9b2d65bf1810d2d6609a32128a00b61d83
BlkerGuy <2020.gafarov@gmail.com>
2023-02-01 04:05:55 +0400

    Add project files.

---
+++ InClassWorks/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+
+namespace InClassWorks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] nums = new int[] {5,10,7,12 };
+            //Console.WriteLine(Sumarr(nums));
+
+
+            //Console.WriteLine(Quvvet(2,3));
+
+
+            //Console.WriteLine(hasChar("Azerbaycan"));
+            //Console.WriteLine(charCount("Hikmet"));
+
+            //Console.WriteLine(Reverse("Hikmet"));
+
+            //Console.WriteLine(FindStr("Azerbaycan",'a'));
+            //Console.WriteLine(FindStr("Azerbaycan", 'a'));
+
+            //int[] arr = new int[] { 20,5,10,12,5};
+            //Console.WriteLine(FindArr(arr,5));
+
+            //Console.WriteLine(findArrEmptyIndex("Salam Hikmet necesen?", "Salam"));
+
+        }
+        //static int Sumarr(int[] nums)
+        //{
+        //    int sum = 0;
+        //    for (int i = 0; i < nums.Length; i++)
+        //    {
+        //        sum += nums[i];
+        //    }
+        //    return sum;
+        //}
+        //static int Quvvet(int eded,int quvvet)
+        //{
+        //    int result = 1;
+        //    for (int i = 0; i < quvvet; i++)
+        //    {
+        //        result *=eded;
+        //    }
+        //    return result;
+        //}
+
+        //static double Quvvet(double eded,double quvvet)
+        //{
+        //    double result = 1;
+        //    for (int i = 0; i < quvvet; i++)
+        //    {
+        //        result *= eded;
+        //    }
+        //    return result;
+        //}
+
+        //static int charCount(string word)
+        //{
+        //    int count = 0;
+
+        //    for (int i = 0; i < word.Length; i++)
+        //    {
+        //        if (word[i] == 'a')
+        //        {
+        //            count++;
+        //        }
+        //    }
+        //    return count;
+        //}
+
+        //static bool hasChar(string word)
+        //{
+
+        //    for (int i = 0; i < word.Length; i++)
+        //    {
+        //        if (word[i] == 'a')
+        //        {
+        //            return true;
+        //        }
+        //    }
+        //    return false;
+        //}
+
+
+        //static string Reverse(string word)
+        // {
+        //     string emptyWord = "";
+        //     for (int i = word.Length-1; i >=0; i--)
+        //     {
+        //         emptyWord += word[i];
+        //     }
+        //     return emptyWord.ToLower();
+        // }
+
+
+        //static int FindStr(string word , char single )
+        //{
+        //    int count = 0;
+        //    for (int i = 0; i < word.Length; i++)
+        //    {
+        //        if (word[i]==single)
+        //        {
+        //            count++;
+        //        }
+        //    }
+        //    return count;
+
+        //}
+
+        //static bool FindStr(string word, char single)
+        //{
+        //    for (int i = 0; i < word.Length; i++)
+        //    {
+        //        if (word[i] == single)
+        //        {
+        //            return true;
+        //        }
+        //    }
+        //    return false;
+        //}
+
+        //static int FindArr(int[] nums,int n)
+        //{
+
+        //    for (int i = 0; i < nums.Length; i++)
+        //    {
+        //        if (nums[i]==n)
+        //        {
+        //            return i;
+        //        }
+        //    }
+        //    return -1;
+        //}
+        //static string findArrEmptyIndex(string word, string findStr)
+        //{
+        //    string FixWord = "";
+
+        //    for (int i = 0; i < word.Length; i++)
+        //    {
+        //        if (word[i]==' ')
+        //        {
+        //            return FixWord;
+        //        }
+        //        FixWord += word[i];
+        //    }
+        //    return FixWord;
+        //}
+    }
+}
---
+++ Method_practice_reference_and_value_types/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Reflection.Emit;
+
+namespace Method_practice_reference_and_value_types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1  -Verilmiş yazının əvvəlindəki boşluqları silib qayataran metod:
+
+            ///Console.WriteLine(rmEmptyLine("  Azerbaycan Torpaqlari Artiq Azaddir."));
+            ///
+            Console.WriteLine(rmEmptyLine("      Elcin Mellimden birdenedir)")); 
+
+            //  Task 2  -Verilmiş ədədlər siyahısından yalnız müsbət ədədlərindən ibarət yeni bir array düzəldib qaytaran metod
+
+            //int[] numbers = new int[] { -3,5, 19,-22 };
+
+            //positivNums(numbers);
+        }
+
+        // Method of Task 1
+
+        static string rmEmptyLine(string word)
+        {
+            string empty = "";
+            string alphabet = "qwertyuiopasdfghjklzxcvbnm";
+            string newStr = word.ToLower();
+
+            for (int i = 0; i < newStr.Length; i++)
+            {
+                if (word[i] != ' ')
+                {
+                    for (int j = 0; i < alphabet.Length; j++)
+                    {
+                        if (newStr[i] == alphabet[j])
+                        {
+                            for (int l = i; l < word.Length; l++)
+                            {
+                                empty += word[l];
+                            }
+                            return empty;
+                        }
+                    }
+                }
+            }
+            return "0"; // 0 windows sisteminde false kimi islendiyinden ele yazmisam;
+        }
+
+        // Method of Task 2
+
+        //static void positivNums(int[] nums)
+        //{
+        //    int[] positivNumbs = new int[nums.Length];
+        //    int j = 0;
+
+        //    for (int i = 0; i < nums.Length; i++)
+        //    {
+        //        if (nums[i] > 0)
+        //        {
+        //            positivNumbs[j] += nums[i];
+        //            Console.WriteLine(positivNumbs[j]);
+        //            j++;
+        //        }
+        //    }
+        //}
+
+        // Bu taski yazmaq ucun 2saatdan cox vaxt xercledim hec cur return ede bilmedim Kecdiklerimize esasen herseyden istifade etmisem hetta internette bele arasdirdim;
+    }
+}
